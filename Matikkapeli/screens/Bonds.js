998f2328c6e7b9f9f390271c692e2a0cbf4e4ec0
Ereride <143874893+Ereride@users.noreply.github.com>
2024-11-02 13:39:47 +0200

    luotu sivujen rungot ja selaus sivujen välillä

---
+++ Matikkapeli/screens/Bonds.js
@@ -0,0 +1,23 @@
+import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Bonds({ onBack }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hajonta</Text>
+      <Button title="Palaa takaisin" onPress={onBack} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
