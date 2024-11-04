774024751367da452143fcbfa451cef53e531c0e
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-11-04 08:51:43 +0200

    Siirretty yhtälön tuloksen laskeminen omaan funktioon.

--- Matikkapeli/screens/Comparison.js
+++ Matikkapeli/screens/Comparison.js
@@ -19,89 +19,88 @@ export default function Comparison({ onBack }) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  const calculateEquation = (operand1, operand2, isAddition) => { //funktio yhtälön arvon laskemista varten
+    if (isAddition) {//jos yhteenslasku, palautetaan 
+      return operand1+operand2
+    } else {
+      return operand1-operand2
+    }
+  }
+
   const checkAnswer = (answer) => { //funktio vastauksen tarkistukseen
     console.log("")
     console.log("vastaus annettu, vastaus: ", answer)
     let correctAnswer = false //muuttuja, jonka perusteella tehdään lopputoimet
-    let resultOfEquation1 = 0
-    let resultOfEquation2 = 0
+    let resultOfEquation1 = 0 //muuttuja 1. yhtälön lopputulokselle
+    let resultOfEquation2 = 0 //muuttuja 2. yhtälön lopputulokselle
 
-    if (isComparable1Equation){ //jos eka vertailtava on yhtälö lasketaan sen arvo
-      if (isEquation1Addition) { //yhteenlaskussa lasketaan yhteen
-        resultOfEquation1 = equation1Operand1 + equation1Operand2
-      } else { //Muutoin vähennetään
-        resultOfEquation1 = equation1Operand1 - equation1Operand2
-      }
-      //console.log("resultOfEquation1: ", resultOfEquation1)
+    if (isComparable1Equation) { //jos 1. vertailtava on yhtälö lasketaan sen arvo
+      resultOfEquation1=calculateEquation(equation1Operand1, equation1Operand2, isEquation1Addition)
     }
-    if (isComparable2Equation){ //jos toinen vertailtava on yhtälö lasketaan sen arvo
-      if (isEquation2Addition) { // yhteenlaskussa lasketaan yhteen
-        resultOfEquation2 = equation2Operand1 + equation2Operand2
-      }else { // Muutoin vähennetään
-        resultOfEquation2 = equation2Operand1 - equation2Operand2
-      }
-      //console.log("resultOfEquation2: ", resultOfEquation2)
+    if (isComparable2Equation) { //jos 2. vertailtava on yhtälö lasketaan sen arvo
+      resultOfEquation2=calculateEquation(equation2Operand1, equation2Operand2, isEquation2Addition)
     }
 
     //logiikka oikeille vastauksille.
     if (answer === "=") { // jos vastaus on yhtäsuuri
-      if (isComparable1Equation && isComparable2Equation){ 
-        if (resultOfEquation1 === resultOfEquation2){ 
+      if (isComparable1Equation && isComparable2Equation) {
+        if (resultOfEquation1 === resultOfEquation2) {
           correctAnswer = true
         }
-      }else if (isComparable1Equation && !isComparable2Equation){
+      } else if (isComparable1Equation && !isComparable2Equation) {
         if (resultOfEquation1 === randomNumber2)
           correctAnswer = true
-      }else if (!isComparable1Equation && isComparable2Equation){
-        if(randomNumber1 === resultOfEquation2){
+      } else if (!isComparable1Equation && isComparable2Equation) {
+        if (randomNumber1 === resultOfEquation2) {
           correctAnswer = true
         }
-      } else if (!isComparable1Equation && !isComparable2Equation){ 
+      } else if (!isComparable1Equation && !isComparable2Equation) {
         if (randomNumber1 === randomNumber2) {
           correctAnswer = true
-          }
         }
+      }
     } else if ( // jos vastaus on 1. vaihtoehto
-      (answer === 1 && 
+      (answer === 1 &&
         (lookingForBigger && !isComparable1Equation && !isComparable2Equation && randomNumber1 > randomNumber2) ||
         (!lookingForBigger && !isComparable1Equation && !isComparable2Equation && randomNumber1 < randomNumber2) ||
-        (lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 > resultOfEquation2)||
-        (!lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 < resultOfEquation2)||
-        (lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 > randomNumber2)||
-        (!lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 < randomNumber2)||
-        (lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 > resultOfEquation2)||
+        (lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 > resultOfEquation2) ||
+        (!lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 < resultOfEquation2) ||
+        (lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 > randomNumber2) ||
+        (!lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 < randomNumber2) ||
+        (lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 > resultOfEquation2) ||
         (!lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 < resultOfEquation2)
-    )) {
+      )) {
       correctAnswer = true
-    } else if (
-      (answer === 2 && 
+    } else if ( // jos vastaus on 2. vaihtoehto
+      (answer === 2 &&
         (lookingForBigger && !isComparable1Equation && !isComparable2Equation && randomNumber1 < randomNumber2) ||
         (!lookingForBigger && !isComparable1Equation && !isComparable2Equation && randomNumber1 > randomNumber2) ||
-        (lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 < resultOfEquation2)||
-        (!lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 > resultOfEquation2)||
-        (lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 < randomNumber2)||
-        (!lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 > randomNumber2)||
-        (lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 < resultOfEquation2)||
+        (lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 < resultOfEquation2) ||
+        (!lookingForBigger && isComparable1Equation && isComparable2Equation && resultOfEquation1 > resultOfEquation2) ||
+        (lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 < randomNumber2) ||
+        (!lookingForBigger && isComparable1Equation && !isComparable2Equation && resultOfEquation1 > randomNumber2) ||
+        (lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 < resultOfEquation2) ||
         (!lookingForBigger && !isComparable1Equation && isComparable2Equation && randomNumber1 > resultOfEquation2)
-    )) {
+      )) {
       correctAnswer = true
     }
 
-/*    console.log("Muuttujat")
-    console.log("isComparable1Equation: ",isComparable1Equation) 
-    console.log("isComparable2Equation: ",isComparable2Equation) 
-    console.log("randomNumber1: ",randomNumber1) 
-    console.log("randomNumber2: ", randomNumber2)
-    console.log("lookingForBigger: ", lookingForBigger)
-    console.log("equation1Operand1: ", equation1Operand1)
-    console.log("equation1Operand2: ", equation1Operand2)
-    console.log("isEquation1Addition: ", isEquation1Addition)
-    console.log("equation2Operand1: ",equation2Operand1)
-    console.log("equation2Operand2: ",equation2Operand2)
-    console.log("isEquation2Addition: ",isEquation2Addition)
-    console.log("resultOfEquation1: ", resultOfEquation1)
-    console.log("resultOfEquation2: ", resultOfEquation2)
-    console.log("correctAnswer: ", correctAnswer)*/
+    /*    console.log("Muuttujat")
+        console.log("isComparable1Equation: ",isComparable1Equation) 
+        console.log("isComparable2Equation: ",isComparable2Equation) 
+        console.log("randomNumber1: ",randomNumber1) 
+        console.log("randomNumber2: ", randomNumber2)
+        console.log("lookingForBigger: ", lookingForBigger)
+        console.log("equation1Operand1: ", equation1Operand1)
+        console.log("equation1Operand2: ", equation1Operand2)
+        console.log("isEquation1Addition: ", isEquation1Addition)
+        console.log("equation2Operand1: ",equation2Operand1)
+        console.log("equation2Operand2: ",equation2Operand2)
+        console.log("isEquation2Addition: ",isEquation2Addition)
+        console.log("resultOfEquation1: ", resultOfEquation1)
+        console.log("resultOfEquation2: ", resultOfEquation2)
+        console.log("correctAnswer: ", correctAnswer)*/
+
     //lopputoimet
     if (correctAnswer) {
       setComparisonXp(prevComparisonXp => prevComparisonXp + 1) //lisätään piste
@@ -129,8 +128,8 @@ export default function Comparison({ onBack }) {
         setIsComparable2Equation(false)
       }
 
-      if (isComparable1Equation) {
-        if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko lauseke yhteenlasku 
+      if (isComparable1Equation) {//jos 1. vertailtava on yhtälö
+        if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko 1. lauseke yhteenlasku 
           setIsEquation1Addition(true) // jos on, asetetaan trueksi ja arvotaan luvut
           setEquation1Operand1(drawRandomNumber(0, comparisonXp * 0.1 + 1))
           setEquation1Operand2(drawRandomNumber(0, comparisonXp * 0.1 + 1))
@@ -148,12 +147,12 @@ export default function Comparison({ onBack }) {
         }
       }
 
-      if (isComparable2Equation) {
-        if (drawRandomNumber(0, 1) === 1) {
-          setIsEquation2Addition(true)
+      if (isComparable2Equation) {//jos 2. vertailtava on yhtälö
+        if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko 1. lauseke yhteenlasku 
+          setIsEquation2Addition(true)// jos on, asetetaan trueksi ja arvotaan luvut
           setEquation2Operand1(drawRandomNumber(0, comparisonXp * 0.1 + 1))
           setEquation2Operand2(drawRandomNumber(0, comparisonXp * 0.1 + 1))
-        } else {
+        } else { // jos ei, asetetaan falseksi ja arvotaan luvut niin, että tulos on vähintään 0
           setIsEquation2Addition(false)
           const first = drawRandomNumber(0, comparisonXp * 0.1 + 1)
           const second = drawRandomNumber(0, comparisonXp * 0.1 + 1)
@@ -183,20 +182,21 @@ export default function Comparison({ onBack }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Vertailu</Text>
-      <Text>comparisonXp: {comparisonXp}</Text>
-      {lookingForBigger ?
+      <Text style={styles.title}>Vertailu</Text> {/* Otsikon tulostus */}
+      <Text>comparisonXp: {comparisonXp}</Text> {/* Pisteiden tulostus */}
+      {lookingForBigger ? {/* Tulostetaan ohjeet sen perusteella, haetaanko isompaa vaiko pienempää */}
         (/*hyphens ? <Text style={styles.guideBigger}>VA-LIT-SE YH-TÄ-SUU-RI TAI SUU-REM-PI</Text> : */<Text style={styles.guideBigger}>Valitse yhtäsuuri tai suurempi</Text>) :
         (/*hyphens ? <Text style={styles.guideSmaller}>VA-LIT-SE YH-TÄ-SUU-RI TAI PIE-NEM-PI</Text> : */<Text style={styles.guideSmaller}>Valitse yhtäsuuri tai pienempi</Text>)
       }
 
-      {isComparable1Equation ?
+      {isComparable1Equation ? {/* Tulostetaan 1. vertailtava sen mukaan, onko se yhtälö vai numero */ }
         (<Text onPress={() => checkAnswer(1)} style={styles.options} >{equation1Operand1} {isEquation1Addition ? '+' : '-'} {equation1Operand2}</Text>) :
         (<Text onPress={() => checkAnswer(1)} style={styles.options} >{randomNumber1}</Text>)
       }
 
-      <Text onPress={() => checkAnswer("=")} style={styles.options} >=</Text>
-      {isComparable2Equation ?
+      <Text onPress={() => checkAnswer("=")} style={styles.options} >=</Text> {/* Tulostetaan yhtäsuurimerkki */}
+
+      {isComparable2Equation ? {/* Tulostetaan 2. vertailtava sen mukaan, onko se yhtälö vai numero */ }
         (<Text onPress={() => checkAnswer(2)} style={styles.options} >{equation2Operand1} {isEquation2Addition ? '+' : '-'} {equation2Operand2}</Text>) :
         (<Text onPress={() => checkAnswer(2)} style={styles.options} >{randomNumber2}</Text>)
       }
