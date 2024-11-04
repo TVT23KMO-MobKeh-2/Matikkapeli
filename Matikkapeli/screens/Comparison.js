ec0cdfcbe17941a6723e65edeafe2b5a3722d494
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-11-04 14:12:37 +0200

    eriytetty toimintoja apufunktioihin koodin selkeyttämiseksi
    - Siirretty returnin rakenne apufunktioihin.
    - Siirretty yhtälön muodostaminen apufunktioon.
    - Siirretty uusien lukujen arpominen apufunktioon.

--- Matikkapeli/screens/Comparison.js
+++ Matikkapeli/screens/Comparison.js
@@ -3,23 +3,63 @@ import React, { useState } from 'react'
 
 export default function Comparison({ onBack }) {
   const [comparisonXp, setComparisonXp] = useState(0) //tilamuuttuja tämän tehtävän pisteille
-  const [isComparable1Equation, setIsComparable1Equation] = useState(false)
-  const [isComparable2Equation, setIsComparable2Equation] = useState(false)
+  const [isComparable1Equation, setIsComparable1Equation] = useState(false) // tilamuuttuja onko 1. vertailtava yhtälö
+  const [isComparable2Equation, setIsComparable2Equation] = useState(false) // tilamuuttuja onko 2. vertailtava yhtälö
   const [randomNumber1, setRandomNumber1] = useState(0) // tilamuuttuja 1. satunnaisluku
   const [randomNumber2, setRandomNumber2] = useState(0) // tilamuuttuja 2. satunnaisluku
   const [lookingForBigger, setLookingForBigger] = useState(false) // tilamuuttuja, etsitäänkö isompaa vai pienempää arvoa
-  const [equation1Operand1, setEquation1Operand1] = useState(0)
-  const [equation1Operand2, setEquation1Operand2] = useState(0)
-  const [isEquation1Addition, setIsEquation1Addition] = useState(false)
-  const [equation2Operand1, setEquation2Operand1] = useState(0)
-  const [equation2Operand2, setEquation2Operand2] = useState(0)
-  const [isEquation2Addition, setIsEquation2Addition] = useState(false)
+  const [equation1Operand1, setEquation1Operand1] = useState(0) // tilamuuttuja, 1. yhtälön 1. numerolle
+  const [equation1Operand2, setEquation1Operand2] = useState(0) // tilamuuttuja, 1. yhtälön 2. numerolle
+  const [isEquation1Addition, setIsEquation1Addition] = useState(false) // tilamuuttuja, onko 1. yhtälö yhteenlasku
+  const [equation2Operand1, setEquation2Operand1] = useState(0) // tilamuuttuja, 2. yhtälön 1. numerolle
+  const [equation2Operand2, setEquation2Operand2] = useState(0) // tilamuuttuja, 2. yhtälön 2. numerolle
+  const [isEquation2Addition, setIsEquation2Addition] = useState(false) // tilamuuttuja, onko 2. yhtälö yhteenlasku
+  const [hyphens, setHyphens] = useState(false) // väliaikainen tilamuuttuja tavutukselle
 
-  const drawRandomNumber = (min, max) => { // funktio satunnaisluvun arvontaan, palauttaa satunnaisluvun annetulta väliltä
+  // funktio satunnaisluvun arvontaan, palauttaa satunnaisluvun annetulta väliltä
+  const drawRandomNumber = (min, max) => { 
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  const calculateEquation = (operand1, operand2, isAddition) => { //funktio yhtälön arvon laskemista varten
+  const drawNewNumbers = () => {
+    //arvotaan uudet satunnaisluvut
+    setRandomNumber1(drawRandomNumber(0, comparisonXp * 0.2 + 1))
+    setRandomNumber2(drawRandomNumber(0, comparisonXp * 0.2 + 1))
+
+    // Jos comparisonXp on tarpeeksi iso arvotaan haetaanko yhtälöä ja kutsutaan tarvittaessa funktiota sen muodostamiseen 
+    if (comparisonXp > 10) {
+      //vertailtava 1:
+      if (drawRandomNumber(0, 1) === 1) {
+        setIsComparable1Equation(true)
+        generateEquation(setIsEquation1Addition, setEquation1Operand1, setEquation1Operand2)
+      } else {
+        setIsComparable1Equation(false)
+      }
+      //vertailtava 2:
+      if (drawRandomNumber(0, 1) === 1) {
+        setIsComparable2Equation(true)
+        generateEquation(setIsEquation2Addition, setEquation2Operand1, setEquation2Operand2)
+      } else {
+        setIsComparable2Equation(false)
+      }
+    }
+
+    // arvotaan haetaanko seuraavaksi isompaa vai pienempää
+    if (drawRandomNumber(0, 1) === 1) { 
+      setLookingForBigger(true) //haetaan isompaa
+      /*if (speech) {
+        speak("Valitse yhtäsuuri tai suurempi.") //toistetaan tehtävänanto puheena
+      }*/
+    } else {
+      setLookingForBigger(false) //haetaan pienempää
+      /*if (speech) {
+        speak("Valitse yhtäsuuri tai pienempi.") //toistetaan tehtävänanto puheena
+      }*/
+    }
+  }
+  
+  //funktio yhtälön arvon laskemista varten
+  const calculateEquation = (operand1, operand2, isAddition) => { 
     if (isAddition) {//jos yhteenslasku, palautetaan summa
       return operand1+operand2
     } else { //muutoin erotus
@@ -27,8 +67,30 @@ export default function Comparison({ onBack }) {
     }
   }
 
-  const checkAnswer = (answer) => { //funktio vastauksen tarkistukseen
-    console.log("")
+  //funktio yhtälön muodostamiseen
+  const generateEquation = (setIsAddition, setOperand1, setOperand2) => {
+    // arvotaan kaksi lukua väliltä 0-comparisonXp * 0.1 +1
+    const first = drawRandomNumber(0, comparisonXp * 0.1 + 1)
+    const second = drawRandomNumber(0, comparisonXp * 0.1 + 1)
+
+    if (drawRandomNumber(0,1) === 1) { // arvotaan onko yhteenlasku, jos on asetetaan muuttujiin luvut ja true  
+      setIsAddition(true)
+      setOperand1(first)
+      setOperand2(second)
+    } else { //muutoin false ja ensimmäiseen muuttujaan isompi luku ja toiseen pienempi, jotta lopputulos ei ole pienempi kuin 0
+      setIsAddition(false)
+      if (first >= second) {
+        setOperand1(first)
+        setOperand2(second)
+      } else {
+        setOperand1(second)
+        setOperand2(first)
+      }
+    }
+  }
+
+  //funktio vastauksen tarkistukseen
+  const checkAnswer = (answer) => { 
     console.log("vastaus annettu, vastaus: ", answer)
     let correctAnswer = false //muuttuja, jonka perusteella tehdään lopputoimet
     let resultOfEquation1 = 0 //muuttuja 1. yhtälön lopputulokselle
@@ -112,94 +174,63 @@ export default function Comparison({ onBack }) {
       //playSound(incorrect) //toistetaan väärin merkkiääni
     }
 
-    setRandomNumber1(drawRandomNumber(0, comparisonXp * 0.2 + 1)) //arvotaan uusi 1. satunnaisluku
-    setRandomNumber2(drawRandomNumber(0, comparisonXp * 0.2 + 1)) //arvotaan uusi 2. satunnaisluku
-
-    // Jos comparisonXp on tarpeeksi iso, arvotaan haetaanko yhtälöitä ja niiden sisällöt
-    if (comparisonXp > 10) {
-      if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko lauseke yhteenlasku
-        setIsComparable1Equation(true)
-      } else {
-        setIsComparable1Equation(false)
-      }
-      if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko lauseke yhteenlasku
-        setIsComparable2Equation(true)
-      } else {
-        setIsComparable2Equation(false)
-      }
-
-      if (isComparable1Equation) {//jos 1. vertailtava on yhtälö
-        if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko 1. lauseke yhteenlasku 
-          setIsEquation1Addition(true) // jos on, asetetaan trueksi ja arvotaan luvut
-          setEquation1Operand1(drawRandomNumber(0, comparisonXp * 0.1 + 1))
-          setEquation1Operand2(drawRandomNumber(0, comparisonXp * 0.1 + 1))
-        } else {
-          setIsEquation1Addition(false) // jos ei, asetetaan falseksi ja arvotaan luvut niin, että tulos on vähintään 0
-          const first = drawRandomNumber(0, comparisonXp * 0.1 + 1)
-          const second = drawRandomNumber(0, comparisonXp * 0.1 + 1)
-          if (first >= second) {
-            setEquation1Operand1(first)
-            setEquation1Operand2(second)
-          } else {
-            setEquation1Operand1(second)
-            setEquation1Operand2(first)
-          }
-        }
-      }
-
-      if (isComparable2Equation) {//jos 2. vertailtava on yhtälö
-        if (drawRandomNumber(0, 1) === 1) {//arvotaan, onko 1. lauseke yhteenlasku 
-          setIsEquation2Addition(true)// jos on, asetetaan trueksi ja arvotaan luvut
-          setEquation2Operand1(drawRandomNumber(0, comparisonXp * 0.1 + 1))
-          setEquation2Operand2(drawRandomNumber(0, comparisonXp * 0.1 + 1))
-        } else { // jos ei, asetetaan falseksi ja arvotaan luvut niin, että tulos on vähintään 0
-          setIsEquation2Addition(false)
-          const first = drawRandomNumber(0, comparisonXp * 0.1 + 1)
-          const second = drawRandomNumber(0, comparisonXp * 0.1 + 1)
-          if (first >= second) {
-            setEquation2Operand1(first)
-            setEquation2Operand2(second)
-          } else {
-            setEquation2Operand1(second)
-            setEquation2Operand2(first)
-          }
-        }
-      }
-    }
-
-    if (drawRandomNumber(0, 1) === 1) { // arvotaan haetaanko seuraavaksi isompaa vai pienempää
-      setLookingForBigger(true) //haetaan isompaa
-      /*if (speech) {
-        speak("Valitse yhtäsuuri tai suurempi.") //toistetaan tehtävänanto puheena
-      }*/
-    } else {
-      setLookingForBigger(false) //haetaan pienempää
-      /*if (speech) {
-        speak("Valitse yhtäsuuri tai pienempi.") //toistetaan tehtävänanto puheena
-      }*/
-    }
+    //arvotaan uudet numerot
+    drawNewNumbers()
   }
 
+  //funktio ohjeen renderöintiä varten
+  const renderGuide = () => { 
+    const text = lookingForBigger // Tallennetaan muuttujaan teksti sen perusteella, etsitäänkö suurempaa... 
+      ? hyphens // ...ja tavutetaanko teksti
+        ? 'VA-LIT-SE YH-TÄ-SUU-RI TAI SUU-REM-PI'
+        : 'Valitse yhtäsuuri tai suurempi'
+      : hyphens
+        ? 'VA-LIT-SE YH-TÄ-SUU-RI TAI PIE-NEM-PI'
+        : 'Valitse yhtäsuuri tai pienempi';
+
+    const style = lookingForBigger ? styles.guideBigger : styles.guideSmaller; // tallennetaan muuttujaan tyyli sen perusteella, etsitäänkö suurempaa
+
+    //palautetaan teksti tyylillä.
+    return <Text style={style}>{text}</Text> 
+  }
+
+  //funktio vertailtavan renderöintiä varten
+  const renderComparable = (comparableNumber) => {
+    //muuttujat comparableNumber perusteella:
+    // tarkistetaan onko kyseessä yhtälö :
+    const isEquation = comparableNumber === 1 ? isComparable1Equation : isComparable2Equation;
+    // tarkistetaan onko kyseessä yhteenlasku :
+    const isAddition = comparableNumber === 1 ? isEquation1Addition : isEquation2Addition;
+    // asetetaan muuttujaan yhtälön 1. osa :
+    const operand1 = comparableNumber === 1 ? equation1Operand1 : equation2Operand1;
+    // asetetaan muuttujaan yhtälön 2. osa :
+    const operand2 = comparableNumber === 1 ? equation1Operand2 : equation2Operand2;
+    // asetetaan muuttujaan satunnaisluku :
+    const randomNumber = comparableNumber === 1 ? randomNumber1 : randomNumber2;
+  
+    if (isEquation) { //jos on yhtälö, niin palautetaan yhtälön osat ja + tai -
+      return (
+        <Text onPress={() => checkAnswer(comparableNumber)} style={styles.options}>
+          {operand1} <Text>{isAddition ? '+' : '-'}</Text> {operand2}
+        </Text>
+      );
+    } else { // muutoin palautetaan satunnaisluku
+      return (
+        <Text onPress={() => checkAnswer(comparableNumber)} style={styles.options}>
+          {randomNumber}
+        </Text>
+      );
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Vertailu</Text> {/* Otsikon tulostus */}
-      <Text>comparisonXp: {comparisonXp}</Text> {/* Pisteiden tulostus */}
-      {lookingForBigger ? {/* Tulostetaan ohjeet sen perusteella, haetaanko isompaa vaiko pienempää */}
-        (/*hyphens ? <Text style={styles.guideBigger}>VA-LIT-SE YH-TÄ-SUU-RI TAI SUU-REM-PI</Text> : */<Text style={styles.guideBigger}>Valitse yhtäsuuri tai suurempi</Text>) :
-        (/*hyphens ? <Text style={styles.guideSmaller}>VA-LIT-SE YH-TÄ-SUU-RI TAI PIE-NEM-PI</Text> : */<Text style={styles.guideSmaller}>Valitse yhtäsuuri tai pienempi</Text>)
-      }
-
-      {isComparable1Equation ? {/* Tulostetaan 1. vertailtava sen mukaan, onko se yhtälö vai numero */ }
-        (<Text onPress={() => checkAnswer(1)} style={styles.options} >{equation1Operand1} {isEquation1Addition ? '+' : '-'} {equation1Operand2}</Text>) :
-        (<Text onPress={() => checkAnswer(1)} style={styles.options} >{randomNumber1}</Text>)
-      }
-
-      <Text onPress={() => checkAnswer("=")} style={styles.options} >=</Text> {/* Tulostetaan yhtäsuurimerkki */}
-
-      {isComparable2Equation ? {/* Tulostetaan 2. vertailtava sen mukaan, onko se yhtälö vai numero */ }
-        (<Text onPress={() => checkAnswer(2)} style={styles.options} >{equation2Operand1} {isEquation2Addition ? '+' : '-'} {equation2Operand2}</Text>) :
-        (<Text onPress={() => checkAnswer(2)} style={styles.options} >{randomNumber2}</Text>)
-      }
+      <Text style={styles.title}>Vertailu</Text>
+      <Text>comparisonXp: {comparisonXp}</Text> 
+      {renderGuide()}
+      {renderComparable(1)}
+      <Text onPress={() => checkAnswer("=")} style={styles.options} >=</Text> 
+      {renderComparable(2)}
       <Button title="Palaa takaisin" onPress={onBack} />
     </View>
   );
