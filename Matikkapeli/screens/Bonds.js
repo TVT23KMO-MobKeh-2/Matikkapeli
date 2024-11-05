60e5420d2c2def36964f0e9042c448af9397e307
Ereride <143874893+Ereride@users.noreply.github.com>
2024-11-05 18:51:13 +0200

    Input kenttä ja logiikka ajatusta aloitettu

--- Matikkapeli/screens/Bonds.js
+++ Matikkapeli/screens/Bonds.js
@@ -1,8 +1,19 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import Svg, { Line } from 'react-native-svg';
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 export default function Bonds({ onBack }) {
+
+  const levelData = 2;
+  const leftBox = random(0, levelData)
+  const rightBox = levelData - leftBox
+  const [witchBox, setwitchBox] = useState(random(0, 1))
+  const [inputValue1, setInputValue1] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
 
   return (
     <View style={styles.container}>
@@ -13,22 +24,42 @@ export default function Bonds({ onBack }) {
         </View>
       </View>
       <View style={styles.circle}>
-        <Text style={styles.circletext}>1</Text>
+        <Text style={styles.circletext}>{levelData}</Text>
       </View>
-      
+
       <Svg height="300" width="300" style={styles.lineContainer}>
-  {/* Line to first box */}
-  <Line x1="150" y1="160" x2="70" y2="230" stroke="black" strokeWidth="5" />
-  {/* Line to second box */}
-  <Line x1="150" y1="160" x2="230" y2="230" stroke="black" strokeWidth="5" />
-</Svg>
-              
+        <Line x1="150" y1="160" x2="70" y2="230" stroke="black" strokeWidth="5" />
+        <Line x1="150" y1="160" x2="230" y2="230" stroke="black" strokeWidth="5" />
+      </Svg>
+
       <View style={styles.numbers}>
         <View style={styles.number1}>
-          <Text style={styles.numbertext}>1</Text>
+          {
+            witchBox === 0 ? (
+              <TextInput
+              style={styles.input} 
+              value={inputValue1}
+              onChange={setInputValue1}
+              keyboardType='numeric'
+              autoFocus={true}/>
+            ) :(
+              <Text style={styles.numbertext}>{leftBox}</Text>
+            )
+          }
         </View>
         <View style={styles.number2}>
-          <Text style={styles.numbertext}>2</Text>
+        {
+            witchBox === 1 ? (
+              <TextInput
+              style={styles.input} 
+              value={inputValue2}
+              onChange={setInputValue2}
+              keyboardType='numeric'
+              autoFocus={true}/>
+            ) :(
+              <Text style={styles.numbertext}>{rightBox}</Text>
+            )
+          }
         </View>
       </View>
     </View>
@@ -50,23 +81,24 @@ const styles = StyleSheet.create({
   },
   taskbox: {
     backgroundColor: 'yellow',
+    zIndex: 4,
   },
   title: {
     fontSize: 24,
     margin: 20,
   },
   circle: {
-    width: 100,                
-    height: 100,               
-    borderRadius: 50,          
+    width: 100,
+    height: 100,
+    borderRadius: 50,
     backgroundColor: 'lightgreen',
-    justifyContent: 'center',   
-    alignItems: 'center',       
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: 20,
     zIndex: 2,
   },
 
-  circletext:{
+  circletext: {
     fontSize: 40,
     borderColor: 'black',
   },
@@ -81,26 +113,29 @@ const styles = StyleSheet.create({
     zIndex: 3,
   },
   number1: {
-    width: 100,                
-    height: 100,                        
+    width: 100,
+    height: 100,
     backgroundColor: 'lightgray',
-    justifyContent: 'center',   
-    alignItems: 'center',       
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: 20,
-    
+
   },
   number2: {
-    width: 100,                
-    height: 100,                        
+    width: 100,
+    height: 100,
     backgroundColor: 'lightblue',
-    justifyContent: 'center',   
-    alignItems: 'center',       
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: 20,
   },
 
   numbertext: {
     fontSize: 40,
-    
+
+  },
+  input: {
+    fontSize: 40,
   },
 
 });
