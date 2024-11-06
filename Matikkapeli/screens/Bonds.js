2d3f81261e1dd4e72a4962231cb6c7b139b969f7
Ereride <143874893+Ereride@users.noreply.github.com>
2024-11-06 15:28:50 +0200

    Peli toimii vaati hieno säätöä

--- Matikkapeli/screens/Bonds.js
+++ Matikkapeli/screens/Bonds.js
@@ -1,6 +1,7 @@
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Button, StyleSheet, TextInput, ImageBackground } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Svg, { Line } from 'react-native-svg';
+import { Audio } from 'expo-av';
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
@@ -8,77 +9,125 @@ function random(min, max) {
 
 export default function Bonds({ onBack }) {
 
-  const levelData = 2;
-  const leftBox = random(0, levelData)
-  const rightBox = levelData - leftBox
-  const [witchBox, setwitchBox] = useState(random(0, 1))
+  const levelData = 7;
+  const [leftBox, setLeftBox] = useState(0);
+  const [rightBox, setRightBox] = useState(0);
+  const [witchBox, setWitchBox] = useState(random(0, 1));
   const [inputValue1, setInputValue1] = useState('');
   const [inputValue2, setInputValue2] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState()
+  const [sound, setSound] = useState();
+  const [doneTasks, setDoneTasks] = useState(0);
+  
 
-  const checkAnswer = async () => { 
-      if (witchBox === 0){
-        setCorrectAnswer(leftBox)
-        if (correctAnswer===inputValue1){
-          <Text>Oikein</Text>
-        } else {
-          <Text>Väärin</Text>
-        }
+  const correctSound = require('../assets/success.mp3');
+  const wrongSound = require('../assets/fail.mp3');
+  const imagaBG = require('../assets/view6.png')
+
+  const generateNewLevel = () => {
+    const newLeftBox = random(0, levelData);
+    const newRightBox = levelData - newLeftBox;
+    setLeftBox(newLeftBox);
+    setRightBox(newRightBox);
+    setWitchBox(random(0, 1));
+    setInputValue1('');
+    setInputValue2('');
+  };
+
+  useEffect(() => {
+    generateNewLevel();
+  }, [levelData]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
+
+  const playSound = async (isCorrect) => {
+    const soundToPlay = isCorrect ? correctSound : wrongSound;
+    const { sound } = await Audio.Sound.createAsync(soundToPlay);
+    setSound(sound);
+    await sound.playAsync();
+  };
+
+  const checkAnswer = async () => {
+    const correctAnswer = witchBox === 0 ? leftBox : rightBox;
+    const userAnswer = witchBox === 0 ? parseInt(inputValue1) : parseInt(inputValue2);
+
+    if (userAnswer === correctAnswer) {
+      await playSound(true);
     } else {
-        setCorrectAnswer(rightBox)
+      await playSound(false);
     }
 
-  }
+    if(doneTasks < 4) {
+      setDoneTasks(doneTasks +1)
+      setTimeout(() => {
+        generateNewLevel()
+      }, 2000) 
+    } else {
+      setTimeout(() => {
+        onBack()
+      }, 3000) 
+    }
+    
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.taskbox}>
-        <Text style={styles.title}>Hajonta</Text>
-        <View style={styles.buttonContainer}>
-          <Button title="Palaa takaisin" onPress={onBack} />
+    <ImageBackground
+      source={imagaBG}
+      style={styles.background}
+      resizeMode='cover'>
+      <View style={styles.container}>
+        <View style={styles.taskbox}>
+          <Text style={styles.title}>Hajonta</Text>
+          <View style={styles.buttonContainer}>
+            <Button title="Palaa takaisin" onPress={onBack} />
+          </View>
         </View>
-      </View>
-      <View style={styles.circle}>
-        <Text style={styles.circletext}>{levelData}</Text>
-      </View>
+        <View style={styles.circle}>
+          <Text style={styles.circletext}>{levelData}</Text>
+        </View>
 
-      <Svg height="300" width="300" style={styles.lineContainer}>
-        <Line x1="150" y1="160" x2="70" y2="230" stroke="black" strokeWidth="5" />
-        <Line x1="150" y1="160" x2="230" y2="230" stroke="black" strokeWidth="5" />
-      </Svg>
+        <Svg height="300" width="300" style={styles.lineContainer}>
+          <Line x1="150" y1="130" x2="70" y2="230" stroke="black" strokeWidth="5" />
+          <Line x1="150" y1="130" x2="230" y2="230" stroke="black" strokeWidth="5" />
+        </Svg>
 
-      <View style={styles.numbers}>
-        <View style={styles.number1}>
-          {
-            witchBox === 0 ? (
+        <View style={styles.numbers}>
+          <View style={styles.number1}>
+            {witchBox === 0 ? (
               <TextInput
-              style={styles.input} 
-              value={inputValue1}
-              onChange={setInputValue1}
-              keyboardType='numeric'
-              autoFocus={true}/>
-            ) :(
+                style={styles.input}
+                value={inputValue1}
+                onChangeText={setInputValue1}
+                keyboardType="numeric"
+                autoFocus={true}
+              />
+            ) : (
               <Text style={styles.numbertext}>{leftBox}</Text>
-            )
-          }
-        </View>
-        <View style={styles.number2}>
-        {
-            witchBox === 1 ? (
+            )}
+          </View>
+          <View style={styles.number2}>
+            {witchBox === 1 ? (
               <TextInput
-              style={styles.input} 
-              value={inputValue2}
-              onChange={setInputValue2}
-              keyboardType='numeric'
-              autoFocus={true}/>
-            ) :(
+                style={styles.input}
+                value={inputValue2}
+                onChangeText={setInputValue2}
+                keyboardType="numeric"
+                autoFocus={true}
+              />
+            ) : (
               <Text style={styles.numbertext}>{rightBox}</Text>
-            )
-          }
+            )}
+          </View>
         </View>
+        <Button title="Tarkista" onPress={checkAnswer}></Button>
+
       </View>
-      <Button title='Tarkista'></Button>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -89,6 +138,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  background: {
+    height: '100%',
+    width:'100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   lineContainer: {
     position: 'absolute',
     justifyContent: 'center',
@@ -96,7 +151,6 @@ const styles = StyleSheet.create({
     zIndex: 1,
   },
   taskbox: {
-    backgroundColor: 'yellow',
     zIndex: 4,
   },
   title: {
@@ -107,51 +161,44 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: 'lightgreen',
+    backgroundColor: '#36BA98',
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
     zIndex: 2,
   },
-
   circletext: {
     fontSize: 40,
     borderColor: 'black',
+    color: 'white',
   },
-
   numbers: {
     flexDirection: 'row',
-    fontSize: 40,
     margin: 20,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    width: '80%',
     zIndex: 3,
   },
   number1: {
     width: 100,
     height: 100,
-    backgroundColor: 'lightgray',
+    backgroundColor: '#E9C46A',
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
-
+    borderRadius: 20,
   },
   number2: {
     width: 100,
     height: 100,
-    backgroundColor: 'lightblue',
+    backgroundColor: '#F4A261',
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
+    borderRadius: 20,
   },
-
   numbertext: {
     fontSize: 40,
-
   },
   input: {
     fontSize: 40,
   },
-
 });
