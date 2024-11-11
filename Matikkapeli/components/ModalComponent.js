2603c42f8e9997ba3eb7c6dc5aa8c267ed97381c
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-11-11 17:26:22 +0200

    Modal-komponentti luotu

---
+++ Matikkapeli/components/ModalComponent.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ModalComponent() {
+  return (
+    <View>
+      <Text>ModalComponent</Text>
+    </View>
+  )
+}
