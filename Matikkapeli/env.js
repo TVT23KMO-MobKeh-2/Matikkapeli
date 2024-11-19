3fab658ba218bbd2f6168aa12e8eb8beef7634ae
Ereride <143874893+Ereride@users.noreply.github.com>
2024-11-19 13:15:57 +0200

    animaatio toimii

---
+++ Matikkapeli/env.js
@@ -0,0 +1,10 @@
+const ENV = {
+    FIREBASE_API_KEY : "AIzaSyBk0J2bckjRDjUe-25aMak_Jmtw0lsdpiQ",
+    FIREBASE_AUTH_DOMAIN : "matikkapeli-6e78b.firebaseapp.com",
+    FIREBASE_PROJECT_ID : "matikkapeli-6e78b",
+    FIREBASE_STORAGE_BUCKET : "matikkapeli-6e78b.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID : "805696573208",
+    FIREBASE_APP_ID : "1:805696573208:web:bdd14a575a7afa3313986d"
+  };
+  
+  export default ENV;
