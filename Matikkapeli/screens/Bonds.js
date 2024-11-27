2d1a734e92d089ffb1bae736c72998dd6f4f1f39
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-11-27 18:54:54 +0200

    Modalia poistettu käytöstä
    feedbackMsg pitää siirtää yhteiseen tiedostoon ja puhe lisätä myös palautteesta. Pitäisi kuitenkin toimia muuten..

--- Matikkapeli/screens/Bonds.js
+++ Matikkapeli/screens/Bonds.js
@@ -4,7 +4,6 @@ import Svg, { Line } from 'react-native-svg';
 import { Audio } from 'expo-av';
 import styles from '../styles';
 import * as Speech from 'expo-speech';
-import ModalComponent from '../components/ModalComponent';
 import { ScoreContext } from '../components/ScoreContext';
 import { useTheme } from '../components/ThemeContext';
 import { useSoundSettings } from '../components/SoundSettingsContext';
@@ -24,13 +23,13 @@ export default function Bonds({ onBack }) {
   const { profile } = route.params;
   const navigation = useNavigation()
 
- useEffect(() => {
-  if (profile) {
-    console.log("Bonds:", profile);
-  } else {
-    console.log("Profile is undefined or null");
-  }
-}, [profile]);
+  useEffect(() => {
+    if (profile) {
+      console.log("Bonds:", profile);
+    } else {
+      console.log("Profile is undefined or null");
+    }
+  }, [profile]);
 
   useEffect(() => {
     console.log("showButtons state:", showButtons);
@@ -45,9 +44,9 @@ export default function Bonds({ onBack }) {
   const [inputValue2, setInputValue2] = useState('');  // Käyttäjän syöte oikeaan laatikkoon
   const [sound, setSound] = useState();  // Äänet, joita toistetaan vastauksen perusteella
   const [doneTasks, setDoneTasks] = useState(0);  // Tavoitteena on vastata oikein 5 kysymykseen
-  const {points, setPoints, questionsAnswered, setQuestionsAnswered, incrementXp, handleUpdatePlayerStatsToDatabase } = useContext(ScoreContext);  // Pelin pistetilanne ja XP:n käsittely
+  const { points, setPoints, questionsAnswered, setQuestionsAnswered, incrementXp, handleUpdatePlayerStatsToDatabase, imageToNumberXp, soundToNumberXp, bondsXp, comparisonXp, playerLevel, totalXp } = useContext(ScoreContext);  // Pelin pistetilanne ja XP:n käsittely
   const [instructionVisibility, setInstructionVisibility] = useState(true);  // Näytetäänkö ohjeet pelin alussa
-  const [modalVisible, setModalVisible] = useState(false);  // Modal-ikkuna näkyvissä vai ei
+  const [showFeedback, setShowFeedback] = useState(false)
   const { isDarkTheme } = useTheme();  // Teeman väri (tumma vai vaalea)
   const { gameSounds } = useSoundSettings();  // Pelin ääniasetukset
   const { taskReading } = useTaskReading();  // Käytetäänkö tehtävän lukemista ääneen
@@ -61,6 +60,33 @@ export default function Bonds({ onBack }) {
   const wrongSound = require('../assets/sounds/mixkit-losing-bleeps.wav');  // Väärä vastausääni
   const imagaBG = require('../assets/view6.png')  // Taustakuva
 
+    //feedback miten meni, odotelee tässä, että saadaan yhteiseen tiedostoon..
+    const feedbackMsg = (() => {
+      switch (points) {
+          case 0:
+              return (!taskSyllabification) ? "0/5 Hyvä, että yritit! Matikka on välillä tosi haastavaa. Harjoitellaan yhdessä lisää, niin ensi kerralla voi mennä paremmin!"
+                  : "0/5 HY-VÄ ET-TÄ Y-RI-TIT! MA-TIK-KA ON VÄ-LIL-LÄ TO-SI HAAS-TA-VAA. HAR-JOI-TEL-LAAN YH-DES-SÄ LI-SÄÄ, NIIN EN-SI KER-RAL-LA VOI MEN-NÄ PA-REM-MIN";
+          case 1:
+              return (!taskSyllabification) ? "1/5 Hyvä, sait yhden oikein! Tämä on hyvä alku, ja joka kerta opit vähän lisää. Kokeillaan yhdessä uudelleen!"
+                  : "1/5 HY-VÄ, SAIT YH-DEN OI-KEIN! TÄ-MÄ ON HY-VÄ AL-KU JA JO-KA KER-TA O-PIT VÄ-HÄN LI-SÄÄ. KO-KEIL-LAAN YH-DES-SÄ UU-DEL-LEEN!";
+          case 2:
+              return (!taskSyllabification) ? "2/5 Hienoa, sait jo kaksi oikein! Olet oppimassa. Jatketaan harjoittelua, niin ensi kerralla osaat vielä enemmän!"
+                  : "2/5 HIE-NOA, SAIT JO KAK-SI OI-KEIN! O-LET OP-PI-MAS-SA. JAT-KE-TAAN HAR-JOIT-TE-LU-A, NIIN EN-SI KER-RAL-LA O-SAAT VIE-LÄ E-NEM-MÄN!";
+          case 3:
+              return (!taskSyllabification) ? "3/5 Mahtavaa, sait yli puolet oikein! Olet jo tosi lähellä. Harjoitellaan vielä vähän, niin pääset vieläkin pidemmälle!"
+                  : "3/5 MAH-TA-VAA, SAIT Y-LI PUO-LET OI-KEIN! O-LET JO TO-SI LÄ-HEL-LÄ. HAR-JOI-TEL-LAAN VIE-LÄ VÄ-HÄN, NIIN PÄÄ-SET VIE-LÄ-KIN PI-DEM-MÄL-LE";
+          case 4:
+              return (!taskSyllabification) ? "4/5 Tosi hienoa! Melkein kaikki meni oikein. Vielä vähän harjoittelua, niin voit saada kaikki oikein ensi kerralla!"
+                  : "4/5 TO-SI HIE-NO-A! MEL-KEIN KAIK-KI ME-NI OI-KEIN. VIE-LÄ VÄ-HÄN HAR-JOIT-TE-LUA, NIIN VOIT SAA-DA KAIK-KI OI-KEIN EN-SI KER-RAL-LA";
+          case 5:
+              return (!taskSyllabification) ? "5/5 Wau, ihan huippua! Sait kaikki oikein! Jatka samaan malliin, olet tosi taitava!"
+                  : "5/5 WAU, I-HAN HUIP-PUA! SAIT KAIK-KI OIK-EIN! JAT-KA SA-MAAN MAL-LIIN, O-LET TO-SI TAI-TA-VA!";
+          default:
+              return (!taskSyllabification) ? "Tässä tämän hetkiset pisteesi:"
+                  : "TÄ-MÄN HET-KI-SET PIS-TEE-SI";
+      }
+  })();
+  
   // Funktio, joka generoi uuden pelitason (arvot vasemmalle ja oikealle laatikolle)
   const generateNewLevel = () => {
     setIsTaskChanging(true)
@@ -102,10 +128,10 @@ export default function Bonds({ onBack }) {
   useEffect(() => {
     if (questionsAnswered === 5) {
       setShowButtons(true)
-      setModalVisible(true);  // Näytetään modal-ikkuna, kun 5 kysymystä on vastattu
-       // Lisätään XP-pisteet
-       incrementXp(points, "bonds");
-       console.log("Bonds points: ",points) 
+      setShowFeedback(true);  // Näytetään feedback-ikkuna, kun 5 kysymystä on vastattu
+      // Lisätään XP-pisteet
+      incrementXp(points, "bonds");
+      console.log("Bonds points: ", points)
     }
   }, [questionsAnswered]);
 
@@ -139,26 +165,26 @@ export default function Bonds({ onBack }) {
   // Takaisin-napin toiminnallisuus
   const handleBack = () => {
     Speech.stop();  // Pysäytetään mahdollinen puhe
-    setModalVisible(false);  // Piilotetaan modal-ikkuna
+    setShowFeedback(false);  // Piilotetaan feedback-ikkuna
     setShowButtons(false)
     setQuestionsAnswered(0);  // Nollataan kysymykset
     setPoints(0);  // Nollataan pisteet
     handleUpdatePlayerStatsToDatabase()
   };
 
-  const isButtonDisabled = 
-  (witchBox === 0 && inputValue1 === '') || 
-  (witchBox === 1 && inputValue2 === '') ||
-  isTaskChanging
+  const isButtonDisabled =
+    (witchBox === 0 && inputValue1 === '') ||
+    (witchBox === 1 && inputValue2 === '') ||
+    isTaskChanging
 
   const handleContinueGame = () => {
     handleBack(); // Actually call handleBack
-    navigation.navigate('Animation', { profile }); 
+    navigation.navigate('Animation', { profile });
   };
 
   const handleEndGame = () => {
     handleBack(); // Actually call handleBack
-    navigation.navigate('SelectProfile', { profile }); 
+    navigation.navigate('SelectProfile', { profile });
   };
 
   return (
@@ -225,26 +251,40 @@ export default function Bonds({ onBack }) {
         </View>
 
         {
-  showButtons ? (
-    <View style={styles.buttonContainer}>
-      <Button title="Seuraava tehtävä odottaa" onPress={handleContinueGame} />
-      <Button title="Lopeta peli" onPress={handleEndGame} />
-    </View>
-  ) : (
-    <Pressable
-      onPress={checkAnswer}
-      style={[styles.checkButton, isButtonDisabled ? styles.disabledButton : null]}
-      disabled={isButtonDisabled}>
-      <Text style={styles.checkButtonText}>Tarkista</Text>
-    </Pressable>
-  )
-}
+          showButtons ? (
+            <View style={styles.buttonContainer}>
+              <Button title="Seuraava tehtävä odottaa" onPress={handleContinueGame} />
+              <Button title="Lopeta peli" onPress={handleEndGame} />
+            </View>
+          ) : (
+            <Pressable
+              onPress={checkAnswer}
+              style={[styles.checkButton, isButtonDisabled ? styles.disabledButton : null]}
+              disabled={isButtonDisabled}>
+              <Text style={styles.checkButtonText}>Tarkista</Text>
+            </Pressable>
+          )
+        }
 
-        <ModalComponent
-          isVisible={modalVisible}
-          profile={profile}
-          onBack={() => setModalVisible(false)}  // Dynamically pass the state updater
-        />
+        {showFeedback && (
+          <TouchableWithoutFeedback>
+            <View style={styles.overlayInstruction}>
+              <View style={styles.instructionWindow}>
+                <Text >{feedbackMsg}</Text>
+                <Text style={styles.title}>Pistetaulu</Text>
+                <Text>Level: {playerLevel}/10</Text>
+                <Text>Kokonaispisteet: {totalXp}/190</Text>
+                <Text>ImageToNumbers: {imageToNumberXp}/50</Text>
+                <Text>SoundToNumbers: {soundToNumberXp}/50</Text>
+                <Text>Comparison: {comparisonXp}/50</Text>
+                <Text>Bonds: {bondsXp}/40</Text>
+                <View style={styles.buttonContainer}>
+                  <Button title='Jatka' onPress={() => setShowFeedback(false)}></Button>
+                </View>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
       </View>
     </ImageBackground>
   );
--- Matikkapeli/screens/SoundToNumber.js
+++ Matikkapeli/screens/SoundToNumber.js
@@ -1,7 +1,6 @@
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Text, Button, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import React, { useState, useContext, useEffect } from 'react';
 import * as Speech from 'expo-speech';
-import ModalComponent from '../components/ModalComponent'
 import { ScoreContext } from '../components/ScoreContext';
 import styles from '../styles';
 import { Audio } from 'expo-av';
@@ -16,19 +15,46 @@ export default function SoundToNumber({ onBack }) {
 
   const [number, setNumber] = useState(generateRandomNumber());
   const [options, setOptions] = useState(generateOptions(number));
-  const [modalVisible, setModalVisible] = useState(false);
-  const {playerLevel,points,setPoints,questionsAnswered,setQuestionsAnswered,incrementXp} = useContext(ScoreContext) // tuodaan tarvittavat muuttujat ja setterit
+  const [showFeedback, setShowFeedback] = useState(false)
+  const { playerLevel, points, setPoints, questionsAnswered, setQuestionsAnswered, incrementXp, handleUpdatePlayerStatsToDatabase, imageToNumberXp, soundToNumberXp, bondsXp, comparisonXp, totalXp } = useContext(ScoreContext) // tuodaan tarvittavat muuttujat ja setterit
   const [sound, setSound] = useState();
   const [gameEnded, setGameEnded] = useState(false);
   const [loading, setLoading] = useState(false);
   const [showButtons, setShowButtons] = useState(false);
 
+    //feedback miten meni, odotelee tässä, että saadaan yhteiseen tiedostoon..
+    const feedbackMsg = (() => {
+      switch (points) {
+          case 0:
+              return (!taskSyllabification) ? "0/5 Hyvä, että yritit! Matikka on välillä tosi haastavaa. Harjoitellaan yhdessä lisää, niin ensi kerralla voi mennä paremmin!"
+                  : "0/5 HY-VÄ ET-TÄ Y-RI-TIT! MA-TIK-KA ON VÄ-LIL-LÄ TO-SI HAAS-TA-VAA. HAR-JOI-TEL-LAAN YH-DES-SÄ LI-SÄÄ, NIIN EN-SI KER-RAL-LA VOI MEN-NÄ PA-REM-MIN";
+          case 1:
+              return (!taskSyllabification) ? "1/5 Hyvä, sait yhden oikein! Tämä on hyvä alku, ja joka kerta opit vähän lisää. Kokeillaan yhdessä uudelleen!"
+                  : "1/5 HY-VÄ, SAIT YH-DEN OI-KEIN! TÄ-MÄ ON HY-VÄ AL-KU JA JO-KA KER-TA O-PIT VÄ-HÄN LI-SÄÄ. KO-KEIL-LAAN YH-DES-SÄ UU-DEL-LEEN!";
+          case 2:
+              return (!taskSyllabification) ? "2/5 Hienoa, sait jo kaksi oikein! Olet oppimassa. Jatketaan harjoittelua, niin ensi kerralla osaat vielä enemmän!"
+                  : "2/5 HIE-NOA, SAIT JO KAK-SI OI-KEIN! O-LET OP-PI-MAS-SA. JAT-KE-TAAN HAR-JOIT-TE-LU-A, NIIN EN-SI KER-RAL-LA O-SAAT VIE-LÄ E-NEM-MÄN!";
+          case 3:
+              return (!taskSyllabification) ? "3/5 Mahtavaa, sait yli puolet oikein! Olet jo tosi lähellä. Harjoitellaan vielä vähän, niin pääset vieläkin pidemmälle!"
+                  : "3/5 MAH-TA-VAA, SAIT Y-LI PUO-LET OI-KEIN! O-LET JO TO-SI LÄ-HEL-LÄ. HAR-JOI-TEL-LAAN VIE-LÄ VÄ-HÄN, NIIN PÄÄ-SET VIE-LÄ-KIN PI-DEM-MÄL-LE";
+          case 4:
+              return (!taskSyllabification) ? "4/5 Tosi hienoa! Melkein kaikki meni oikein. Vielä vähän harjoittelua, niin voit saada kaikki oikein ensi kerralla!"
+                  : "4/5 TO-SI HIE-NO-A! MEL-KEIN KAIK-KI ME-NI OI-KEIN. VIE-LÄ VÄ-HÄN HAR-JOIT-TE-LUA, NIIN VOIT SAA-DA KAIK-KI OI-KEIN EN-SI KER-RAL-LA";
+          case 5:
+              return (!taskSyllabification) ? "5/5 Wau, ihan huippua! Sait kaikki oikein! Jatka samaan malliin, olet tosi taitava!"
+                  : "5/5 WAU, I-HAN HUIP-PUA! SAIT KAIK-KI OIK-EIN! JAT-KA SA-MAAN MAL-LIIN, O-LET TO-SI TAI-TA-VA!";
+          default:
+              return (!taskSyllabification) ? "Tässä tämän hetkiset pisteesi:"
+                  : "TÄ-MÄN HET-KI-SET PIS-TEE-SI";
+      }
+  })();
+  
   //Koukku jolla tarkistetaan joko kierros päättyy.
   useEffect(() => {
-    if(questionsAnswered===5){
+    if (questionsAnswered === 5) {
       Speech.stop(); //pysäyttää puheen
-      incrementXp(points,"soundToNumber") //comparisonin tilalle oma tehtävän nimi: "imageToNumber", "soundToNumber", "comparison" tai "bonds"
-      setModalVisible(true)
+      incrementXp(points, "soundToNumber") //comparisonin tilalle oma tehtävän nimi: "imageToNumber", "soundToNumber", "comparison" tai "bonds"
+      setShowFeedback(true)
       setGameEnded(true)
       setShowButtons(true)
     }
@@ -36,9 +62,10 @@ export default function SoundToNumber({ onBack }) {
 
   const handleBack = () => {
     Speech.stop()
-    setModalVisible(false);
+    setShowFeedback(false);
     setQuestionsAnswered(0);
     setPoints(0);
+    handleUpdatePlayerStatsToDatabase()
   };
   /*useEffect(() => {
     if (loading) {
@@ -61,24 +88,24 @@ export default function SoundToNumber({ onBack }) {
     const soundUri = isCorrect
       ? require('../assets/sounds/mixkit-achievement-bell.wav')
       : require('../assets/sounds/mixkit-losing-bleeps.wav');
- 
+
     const { sound } = await Audio.Sound.createAsync(soundUri);
     setSound(sound);
     await sound.playAsync();
     sound.setOnPlaybackStatusUpdate(async (status) => {
       if (status.didJustFinish) {
-        sound.unloadAsync(); 
+        sound.unloadAsync();
         setLoading(false); // napit takaisin käyttöön
       }
     });
   };
-  
-//tässä vaiheessa vielä generoi randomilla numeron 1-10 väliltä
+
+  //tässä vaiheessa vielä generoi randomilla numeron 1-10 väliltä
   function generateRandomNumber() {
-    return Math.floor(Math.random() * 10); 
+    return Math.floor(Math.random() * 10);
   }
- 
-//valitsee oikean numeron ja 3 muuta randomilla
+
+  //valitsee oikean numeron ja 3 muuta randomilla
   function generateOptions(correctNumber) {
     const options = [correctNumber];
     while (options.length < 3) {
@@ -88,14 +115,14 @@ export default function SoundToNumber({ onBack }) {
       }
     }
     //sekoittaa vaihtoehdot
-    return options.sort(() => Math.random() - 0.5); 
+    return options.sort(() => Math.random() - 0.5);
   }
 
   const playNumber = () => {
     Speech.stop();
     Speech.speak(number.toString());
   };
-  
+
   const handleSelect = (selectedNumber) => {
     if (gameEnded) return;
     Speech.stop();
@@ -111,7 +138,7 @@ export default function SoundToNumber({ onBack }) {
     }
     setQuestionsAnswered((prevQuestionsAnswered) => prevQuestionsAnswered + 1);
   };
-  
+
 
   return (
     <View style={styles.container}>
@@ -129,12 +156,26 @@ export default function SoundToNumber({ onBack }) {
           </TouchableOpacity>
         ))}
       </View>
-      <ModalComponent
-        isVisible={modalVisible}
-        onBack={() => setModalVisible(false)}
-        profile={profile}
-      />
-        {showButtons && (
+      {showFeedback && (
+        <TouchableWithoutFeedback>
+          <View style={styles.overlayInstruction}>
+            <View style={styles.instructionWindow}>
+              <Text >{feedbackMsg}</Text>
+              <Text style={styles.title}>Pistetaulu</Text>
+              <Text>Level: {playerLevel}/10</Text>
+              <Text>Kokonaispisteet: {totalXp}/190</Text>
+              <Text>ImageToNumbers: {imageToNumberXp}/50</Text>
+              <Text>SoundToNumbers: {soundToNumberXp}/50</Text>
+              <Text>Comparison: {comparisonXp}/50</Text>
+              <Text>Bonds: {bondsXp}/40</Text>
+              <View style={styles.buttonContainer}>
+                <Button title='Jatka' onPress={() => setShowFeedback(false)}></Button>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+      {showButtons && (
         <View style={styles.buttonContainer}>
           <Button title="Seuraava tehtävä odottaa" onPress={handleContinueGame} />
           <Button title="Lopeta peli" onPress={handleEndGame} />
